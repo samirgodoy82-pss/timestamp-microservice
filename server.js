158b87234a0024c0366ecacdcf174b0b444dfad6
samirgodoy82-pss <samirgodoy82@gmail.com>
2025-11-20 22:17:58 -0300

    cambio3

--- server.js
+++ server.js
@@ -1,36 +1,18 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
-app.get("/api/:date?", (req, res) => {
-  let date = req.params.date;
+app.use(cors());
 
-  // Si no hay parámetro, usar fecha actual
-  if (!date) {
-    const now = new Date();
-    return res.json({
-      unix: now.getTime(),
-      utc: now.toUTCString()
-    });
-  }
-
-  // Si es un número → timestamp
-  let parsedDate = /^\d+$/.test(date)
-    ? new Date(parseInt(date))
-    : new Date(date);
-
-  // Validar fecha
-  if (parsedDate.toString() === "Invalid Date") {
-    return res.json({ error: "Invalid Date" });
-  }
-
-  // Respuesta correcta
+app.get("/api/whoami", (req, res) => {
   res.json({
-    unix: parsedDate.getTime(),
-    utc: parsedDate.toUTCString()
+    ipaddress: req.ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log("Servidor funcionando en puerto " + port);
+  console.log("Servidor ejecutándose en puerto " + port);
 });
